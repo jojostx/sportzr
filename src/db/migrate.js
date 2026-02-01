0cdc8352e01631df2063b55c5346aff18c14d7f4
jojostx <ikuskid7@gmail.com>
2026-02-01 15:17:34 +0100

    add drizzle, migrations, db setup

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,6 +1,6 @@
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { db } from './db.js'; // Point this to where you initialize your db
-import config from '../../drizzle-config.js'; // Point to your config to get the output path
+import config from '../../drizzle.config.js'; // Point to your config to get the output path
 
 // This will run all migrations in the folder specified in your config
 // If you didn't define 'out' in config, default is './drizzle'
